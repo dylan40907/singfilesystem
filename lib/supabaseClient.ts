da3dccfef6781890757347260a7313e8f40db9a2
dylan40907 <dylansken@gmail.com>
2025-12-23 16:22:52 -0800

    fixing preview issues

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,6 +1,12 @@
+// lib/supabaseClient.ts
 import { createClient } from "@supabase/supabase-js";
 
 const url = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const anonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const anon = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(url, anonKey);
+export const supabase =
+  typeof window === "undefined"
+    ? // prevent server build from exploding if someone accidentally imports this
+      // (still better to NOT import it on server at all)
+      createClient(url, anon)
+    : createClient(url, anon);
